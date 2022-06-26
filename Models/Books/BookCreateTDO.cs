0cbb17fe2ff2b410678f215fd4fbc6a674fe99bd
AhmedAlaa123 <mm1869695@gmail.com>
2022-06-26 22:47:14 +0200

    adding book class and departmet

---
+++ Models/Books/BookCreateTDO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models.Books
+{
+    public class BookCreateTDO
+    {
+        [MinLength(2,ErrorMessage ="*Minimu Length Of Title is 2 charcter")]
+        [MaxLength(50, ErrorMessage = "*Maximum Length Of Title is 50 charcter")]
+        [Required(ErrorMessage ="*Book Title Is Required")]
+        public int Title { get; set; }
+        public int Copies { get; set; }
+        [Required(ErrorMessage = "*Book Edition Is Required")]
+        public string Edition { get; set; }
+        [Required(ErrorMessage = "*Author Name Is Required")]
+
+        public int Author_Id { get; set; }
+        [Required(ErrorMessage = "*Department Name Is Required")]
+        public int Dept_Id { get; set; }
+    }
+}
---
+++ Models/Departments/Department.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Models.Books;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models.Departments
+{
+    public class Department
+    {
+        [Key()]
+  
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [MinLength(4)]
+   
+        public string? Code { get; set; }
+
+        public List<Book> Books { get; set; }
+
+    }
+}
